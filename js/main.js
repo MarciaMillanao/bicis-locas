8f1d06188eb703e7654a9b6e1e744be48f31f296
marcia <marcia.millanao@gmail.com>
2016-10-27 12:56:38 -0300

    revisado seccion seleccion bici

--- js/main.js
+++ js/main.js
@@ -9,66 +9,85 @@ function validateForm(){
 	var emailValido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 	var soloLetras = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
 	//validar nombre
-	var nombre = document.getElementById('name').value;
-	if(nombre == ""){//valida que se ingrese correo
-		alert("Ingrese Nombre");
-		return false;
+	function nombre(){
+		var nombre = document.getElementById('name').value;
+		if(nombre.length == 0){//valida que se ingrese nombre
+			alert("Ingrese Nombre");
+			return false;
+		}
+		if (!soloLetras.test(nombre)){
+			alert("ingrese solo letras");
+			return false;
+		}else if(nombre.substring(0,1) !== nombre.substring(0,1).toUpperCase()){
+			alert("ingrese inicial con mayuscula");
+			return false;
+		}		
 	}
-	if (!soloLetras.test(nombre)){
-		alert("ingrese solo letras");
-		return false;
-	}else if(nombre.substring(0,1) !== nombre.substring(0,1).toUpperCase()){
-		alert("ingrese inicial con mayuscula");
-		return false;
-	}
+	nombre();
+
 	//validar apellido
-	var apellido = document.getElementById('lastname').value;
-	if(apellido == ""){//valida que se ingrese correo
-		alert("Ingrese Apellido");
-		return false;
+	function apellido(){
+		var apellido = document.getElementById('lastname').value;
+		if(apellido.length == 0){//valida que se ingrese correo
+			alert("Ingrese Apellido");
+			return false;
+		}
+		if (!soloLetras.test(apellido)){
+			alert("ingrese solo letras");
+			return false;	
+		}else if(apellido.substring(0,1) !== apellido.substring(0,1).toUpperCase()){
+			alert("ingrese inicial con mayuscula");
+			return false;
+		}
 	}
-	if (!soloLetras.test(apellido)){
-		alert("ingrese solo letras");
-		return false;	
-	}else if(apellido.substring(0,1) !== apellido.substring(0,1).toUpperCase()){
-		alert("ingrese inicial con mayuscula");
-		return false;
-	}
+	apellido();
+
 
 	//validar correo
-	var email = document.getElementById('input-email').value;
-	if(email == ""){//valida que se ingrese correo
-		alert("Ingrese Email");
-		return false;
+	function correo(){
+		var email = document.getElementById('input-email').value;
+		if(email.length == 0){//valida que se ingrese correo
+			alert("Ingrese Email");
+			return false;
+		}
+		if (!emailValido.test(email)){
+			alert ("correo invalido");
+			return false;
+		}
 	}
-	if (!emailValido.test(email)){
-		alert ("correo invalido");
-		return false;
-	}
+	correo();
+
 
 	//validar contraseña 
-	var password = document.getElementById('input-password').value;
-	if(password == ""){//valida que se ingrese correo
-		alert("Ingrese Password");
-		return false;
-	}else if(password === "123456" || password === "098754" || password === "password"){
-        alert("Tu contraseña no puede ser 123456 o 098764 o password");
-        return false;
-    }else if(password.length<6){
-        alert("tu contraseña debe tener mas de 6 caracteres");
-        return false;
-    }else{
-        alert("Contraseña segura");
-        return true;
-    }
-
-	var bicicleta = document.getElementsByTagName('select').selectedIndex;
-	if( indice == null || indice == 0 ) {
-		alert("elija opcion valida")
-  		return false;
+	function contraseña(){
+		var password = document.getElementById('input-password').value;
+		if(password.length == 0){//valida que se ingrese correo
+			alert("Ingrese Password");
+			return false;
+		}else if(password === "123456" || password === "098754" || password === "password"){
+			alert("Tu contraseña no puede ser 123456 o 098764 o password");
+			return false;
+		}else if(password.length<6){
+			alert("tu contraseña debe tener mas de 6 caracteres");
+			return false;
+		}else{
+			alert("Contraseña segura");
+			return true;
+		}
 	}
+	contraseña();
 
-	
+	//seleccionar bicicleta
+	function seleccionBici(){
+		var bicicleta = document.getElementsByTagName('select'); 
+		for (var i = 0; i < bicicleta.length; i++){ //recorre la lista de opciones
+			if(bicicleta[i].value === "0"){
+				alert("Seleccione opcion correcta");
+				return false;
+			}
+		} 
+			return true;
+	}
+	seleccionBici();
 
-	
 }
